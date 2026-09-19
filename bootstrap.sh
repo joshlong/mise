457f33abb18ea2a04a0e14ab853f95ee858a9a7d
Josh Long <joshua.long@broadcom.com>
2026-09-18 23:08:16 -0600

    mise

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-curl https://mise.run | sh
+# curl https://mise.run | sh
 mise use -g bitwarden
-bw login
+bw login 
 export BW_SESSION=$(bw unlock --raw)
 mkdir -p ~/.config/mise
 
 KEY=$HOME/.ssh/mise_private_key
+PUB_KEY=${KEY}.pub
+
 bw get notes "mise-bootstrap-private-ssh-key" > $KEY
+bw get notes "mise-bootstrap-public-ssh-key" > $PUB_KEY
 chmod 600 $KEY
 
-mise settings set age.key_path $KEY
-mise set --age-encrypt A=B
+mise settings experimental=true
+mise set --age-encrypt --age-ssh-recipient $PUB_KEY --prompt DB_PASSWORD
 
 
-mise bootstrap --adopt https://github.com/joshlong/mise.git
+# mise bootstrap --adopt https://github.com/joshlong/mise.git
